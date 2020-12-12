65b111b521cb8fa06567379fe6920180cbef8d4d
AngelHernandez20 <191280@ids.upchiapas.edu.mx>
2020-12-11 22:30:03 -0600

    change config settings for deploy server aws

--- alumnos/settings.py
+++ alumnos/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '()mlt0mae5r!$qn1hs5kz1a&x@k*d+)fk9iu$8jx9-kk$)hx4x'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['34.224.93.86','172.31.22.225']
 
