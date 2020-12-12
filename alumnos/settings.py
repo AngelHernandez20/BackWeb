762a2307c8c63020185b8493eb92c81d075aeaf7
AngelHernandez20 <191280@ids.upchiapas.edu.mx>
2020-12-11 22:24:22 -0600

    changes settings

--- alumnos/settings.py
+++ alumnos/settings.py
@@ -24,17 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = '()mlt0mae5r!$qn1hs5kz1a&x@k*d+)fk9iu$8jx9-kk$)hx4x'
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-<<<<<<< HEAD
 # DEBUG = False
 DEBUG = config('DEBUG', default=False, cast=bool)
-=======
-DEBUG = True
 
-ALLOWED_HOSTS = ['34.224.93.86']
->>>>>>> staging
 
 # ALLOWED_HOSTS = ['172.31.22.225']
-ALLOWED_HOSTS = ["*",'34.224.93.86']
+ALLOWED_HOSTS = ["*",'172.31.22.225']
 
 # Application definition
 
@@ -127,10 +122,10 @@ WSGI_APPLICATION = 'alumnos.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Back_reporte',
-        'USER': 'postgres',
+        'NAME': 'webdb',
+        'USER': 'webuserdb',
         'PASSWORD': 'angel20340',
-        'HOST': '127.0.0.1',
+        'HOST': '172.31.22.225',
         'PORT': '5432',
     }
 }
