1565297e83f21efbf08c0d7ea0d1e6e2f68a1a36
AngelHernandez20 <191280@ids.upchiapas.edu.mx>
2020-12-12 20:29:45 -0600

    HOST

--- alumnos/settings.py
+++ alumnos/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
         'NAME': 'webdb',
         'USER': 'webuserdb',
         'PASSWORD': 'angelhdz20',
-        'HOST': '3.89.207.205',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
